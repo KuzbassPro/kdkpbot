30a528ab999e67ff4ca6845d44ae60c8ef3ea31e
Novoselov Dmitrii <da.novoselov@ya.ru>
2025-06-20 18:17:14 +0300

    fix: make main async and fix run_polling

--- src/kdkpbot/main.py
+++ src/kdkpbot/main.py
@@ -1,21 +1,21 @@
 # src/kdkpbot/main.py
 
 import os
-import asyncio
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def start_handler(update: Update, context: CallbackContext) -> None:
+async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Привет! Бот работает!")
 
-def main():
+async def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start_handler))
-    app.run_polling()
+    await app.run_polling()
 
 if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
